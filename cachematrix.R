192129829f2cfdc39721d96e676b72674af5c1a3
khartig <kevin.hartig@gmail.com>
2015-05-22 20:35:12 -0700

    initial function logic

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,25 @@
 ## This pair of functions caches the inverse of a matrix so unnecessary
 ## calculations are not perfomed. A call to cacheSolve returns the inverse
-## of the matrix passed in asa the first parameter. A cached version of the 
-## inverse is returned if exists. Othersise the matrix inversion calculation
-## is performed and cache for future reference..
+## of the matrix passed in as the first parameter. A cached version of the 
+## inverse is returned if it exists. Othersise the matrix inversion calculation
+## is performed and cached for future reference.
 ## The code assumes the matrix supplied is invertible and square.
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    set_inverse <- function(solve) m <<- solve
+    get_inverse <- function() m
+    list(set = set, 
+         get = get,
+         set_inverse = set_inverse,
+         get_inverse = get_inverse)
 }
 
 
@@ -17,5 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then cacheSolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$get_inverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$set_inverse(m)
+    m
 }
